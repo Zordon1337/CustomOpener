d83708640079b45c5a6beccfa6704a4285a4e6bd
Zordon <65111609+Zordon1337@users.noreply.github.com>
2024-05-13 18:40:26 +0200

    added fetching json from name

--- src/web/editor.js
+++ src/web/editor.js
@@ -1,4 +1,28 @@
-function GetSkinsJson()
+var db = require("../db/manager");
+function GetSkinsJson(value) {
+    return new Promise((resolve, reject) => {
+        db.conn.query(`SELECT * FROM skins WHERE id = ?`, [value], (error, results, fields) => {
+            if (error) {
+                reject(error); 
+            } else {
+                resolve(results); 
+            }
+        });
+    });
+}
+function DrawEditor(req,res,fs)
 {
+    
+    fs.readFile('src/static/editor.html', 'utf8', (err, data) => {
+        if (err) {
+          console.error('Error reading file:', err);
+          res.status(500).json({ error: 'Internal Server Error' });
+          return;
+        }
+        
+        
+        
+    });
+
 }
 module.exports = {GetSkinsJson}
